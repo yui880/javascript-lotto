76945b8c859531ff29740e0a5eeb7b6f302b7fe5
yui880 <kimyuna521@gmail.com>
2023-10-17 13:09:51 +0900

    feat : 각 메세지 상수에 줄바꿈 추가

--- src/Constant.js
+++ src/Constant.js
@@ -1,8 +1,8 @@
 const Constant = {
-  ENTER_MONEY: '구입금액을 입력해 주세요.',
-  PRINT_QUANTITY: '개를 구매했습니다',
-  ENTER_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.',
-  ENTER_BONUS_NUMBER: '보너스 번호를 입력해 주세요.',
+  ENTER_MONEY: '구입금액을 입력해 주세요.\n',
+  PRINT_QUANTITY: '개를 구매했습니다\n',
+  ENTER_WINNING_NUMBERS: '당첨 번호를 입력해 주세요.\n',
+  ENTER_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
 };
 
 module.exports = Constant;
