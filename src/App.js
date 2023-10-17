7856899e6f3638a39896830c6986d75c6641e6ac
yui880 <kimyuna521@gmail.com>
2023-10-17 13:44:23 +0900

    feat : 구매 금액에 대한 예외처리 구현

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 const { Console } = require('@woowacourse/mission-utils');
 const Constant = require('./Constant');
+const Validator = require('./Validator');
 
 class App {
   #money;
@@ -10,8 +11,18 @@ class App {
 
   #getMoney() {
     Console.readLine(Constant.ENTER_MONEY, (inputMoney) => {
+      this.#validateMoney(inputMoney);
       this.#money = inputMoney;
     });
+  }
+
+  #validateMoney(testMoney) {
+    if (!Validator.checkIsNumber(testMoney)) {
+      throw new Error('[Error] 금액이 숫자가 아닙니다.');
+    }
+    if (!Validator.checkIsThousands(testMoney)) {
+      throw new Error('[Error] 금액이 1,000원 단위가 아닙니다.');
+    }
   }
 }
 
