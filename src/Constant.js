423c00da75b8f4e110717095989c556e1a801cae
yui880 <kimyuna521@gmail.com>
2023-10-17 16:21:02 +0900

    refactor: Lotto 예외처리 메세지 상수로 변경

--- src/Constant.js
+++ src/Constant.js
@@ -5,6 +5,7 @@ const CONSTANT = {
   ENTER_BONUS_NUMBER: '보너스 번호를 입력해 주세요.\n',
   ERROR_NOT_A_NUMBER: '[Error] 금액이 숫자가 아닙니다.',
   ERROR_NOT_THOUSANDS: '[Error] 금액이 1,000원 단위가 아닙니다.',
+  ERROR_NOT_SIX_NUMS: '[ERROR] 로또 번호는 6개여야 합니다.',
 };
 
 module.exports = CONSTANT;
--- src/Lotto.js
+++ src/Lotto.js
@@ -1,3 +1,5 @@
+const CONSTANT = require('./Constant');
+
 class Lotto {
   #numbers;
 
@@ -8,11 +10,9 @@ class Lotto {
 
   validate(numbers) {
     if (numbers.length !== 6) {
-      throw new Error("[ERROR] 로또 번호는 6개여야 합니다.");
+      throw new Error(CONSTANT.ERROR_NOT_SIX_NUMS);
     }
   }
-
-  // TODO: 추가 기능 구현
 }
 
 module.exports = Lotto;
